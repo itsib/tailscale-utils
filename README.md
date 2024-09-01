f816268582eff82375362c8de71ca10f0f6ef2bc
Sergey Krainov <itsib.su@gmail.com>
2024-09-01 20:23:22 +0700

    Fixed bypass script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Utilities for the administration of tailscle networks
+# Tailscale utils
+
+Utilities for the administration of tailscale networks.
+
+### Update Service
+
+Systemd service `tailscale-update` supports bypass 451 blocking when updating the system. Error 451 Unavailable For Legal Reasons
--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 SERVICES_PATH="/etc/systemd/system"
 BIN_PATH="/usr/sbin"
 
@@ -10,20 +8,42 @@ function msg {
     i) echo -e "\x1b[0;37m  $2\x1b[0m" ;;
     s) echo -e "\x1b[0;92m✔ $2\x1b[0m" ;;
     e) echo -e "\x1b[0;91m✗ $2\x1b[0m" ;;
-    *) echo -e "\x1b[0;96m🪐 $@\x1b[0m\n" ;;
+    *) echo -e "\x1b[0;96m$1\x1b[0m" ;;
     esac
 }
 
+function usage {
+  cat << EOF
+The script for installing and removing the tail scale update service.
+Support blocking bypass when updating the system (Error 451 Unavailable For Legal Reasons)
+
+USAGE:
+  ./install.sh install
+  ./install.sh remove
+
+EOF
+}
+
+function root_check {
+    local user
+    user=$(id -u)
+
+    if [ "$user" != "0" ]; then
+        msg e "Must be root; use sudo"
+        exit 1
+    fi
+}
+
 function install_service {
-    msg i "Copy the executable files"
+    msg i "Update the system files"
     cp tailscale-update.service "$SERVICES_PATH/tailscale-update.service"
     cp tailscale-update.timer "$SERVICES_PATH/tailscale-update.timer"
     cp tailscale-update "$BIN_PATH/tailscale-update"
-    msg s "Files copied"
+    msg s "System files updated"
 
-    msg i "Verify instalation"
+    msg i "Verify installation"
     systemd-analyze verify /etc/systemd/system/tailscale-update.*
-    msg s "Intalation verified"
+    msg s "Installation verified"
 
     msg i "Enabling & starting services"
     systemctl --quiet enable tailscale-update.timer
@@ -34,12 +54,16 @@ function install_service {
 }
 
 function remove_service {
+    local state
     state=$(systemctl is-enabled tailscale-update.timer 2>&1)
     if [ "$state" == "enabled" ]; then
         msg i "Stopping & disabling services"
         systemctl --quiet stop tailscale-update.timer
         systemctl --quiet disable tailscale-update.timer
         msg s "Services have been stopped"
+    elif [ "$state" == "not-found" ]; then
+        msg e "No installed files were found."
+        exit 1
     else
         msg i "There are no running services. Skip."
     fi
@@ -62,19 +86,21 @@ function remove_service {
     fi
 
     if [ "$is_removed" == "removed" ]; then
-        msg s "The tailscale-update service has been successfully removed"
+        msg s "The tailscale-update service has been successfully removed\n"
+        exit 0
     else
         msg e "No installed files were found."
+        exit 1
     fi
-    echo ""
-    exit 0
 }
 
-msg "Tailscale utils"
+root_check
 
-if [[ "$@" == "install" ]]; then
+msg "🪐 Tailscale: $1"
+
+if [ "$1" == "install" ]; then
     install_service
-elif [[ "$@" == "remove" ]]; then
+elif [ "$1" == "remove" ]; then
     remove_service
 else
     msg e "Action is required, install or remove\n"
